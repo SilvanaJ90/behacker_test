94261fdef36c4f0e3c4060589cdc529d10c6818e
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-02-15 17:13:40 -0500

    register

--- app/static/js/register.js
+++ app/static/js/register.js
@@ -1,14 +1,25 @@
-$('button').click(function() {
+$(document).ready(function () {
+  $("form").submit(function (event) {
+    event.preventDefault();
+    var formData = {
+      first_name : $('#first_name').val(),
+      last_name: $('#last_name').val(),
+      email: $('#email').val(),
+      password: $('#password').val()
+    };
+
     $.ajax({
-      url: 'http://127.0.0.1:5001/api/v1/roles/{rol_id}/users',
-      type: 'POST',
-      contentType: 'application/json',
-      data: JSON.stringify({}),
-      success: (data) => {
-        data.sort(function (a, b) {
-          return a.name.localeCompare(b.name);
-        });
-        $('article').remove();
-        }
-    });
-});
+      url: "http://127.0.0.1:5001/api/v1/roles/dc32135c-1f1e-4dac-8771-1b91289dd03e/users",
+      type: "POST",
+      data: JSON.stringify(formData),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function(res) {
+        console.log(res);
+
+      }
+    })
+
+  });
+
+}); 
