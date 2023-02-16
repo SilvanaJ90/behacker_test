558efce34cede46c127425dcbbce7515eca2b556
Giogap <giogap@gmail.com>
2023-02-16 10:56:56 -0500

    upload login

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
-    $('#login').submit(function(e) {
+    $('button').click(function(e) {
         e.preventDefault();
-        document.getElementById('submitForm').disabled = true;
+        let formData_i = {
+            email: $('#email_i').val(),
+            password: $('#password_i').val()
+          };
+        document.getElementById('button').disabled = true;
         $.ajax({
             type: "POST",
-            url: 'http://127.0.0.1:5001/api/v1/roles/{rol_id}/users',
-            headers: {'Authorization': 'Bearer xxxxxxxxxxxxx'},
-            data: $(this).serialize(),
+            url: "http://127.0.0.1:5001/api/v1/roles/dc32135c-1f1e-4dac-8771-1b91289dd03e/users",
+            data: JSON.stringify(formData_i),
+            contentType: "application/json; charset=utf-8",
             success: function(response)
             {
                alert('Acceso Ok');
-               $('#email').val('');
-               $('#password').val('');
-               document.getElementById('submitForm').disabled = false;
+               $('#email_i').val('');
+               $('#password_i').val('');
+               document.getElementById('button').disabled = false;
            },
            error:function(error){
             console.log(e.message);
