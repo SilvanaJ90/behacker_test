ce61ea17ae248b0e6d264f63949b81d41f865132
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-02-20 15:25:32 -0500

    update

--- app/static/js/test.js
+++ app/static/js/test.js
@@ -1,23 +1,38 @@
-fetch('http://127.0.0.1:5001/api/v1/categories/16222f5a-2979-4fe5-8532-9d410095a093/words')
-.then(response => response.json())
-.then(data => {
-  // Obtener el contenedor de los checkboxes
-  const checkboxContainer = document.getElementById('checkbox-container');
+fetch('http://127.0.0.1:5001/api/v1/categories')
+  .then(response => response.json())
+  .then(categoriesData => {
+    // Obtener el contenedor de los checkboxes
+    const checkboxContainer = document.getElementById('checkbox-container');
 
-  // Construir los checkboxes
-  data.forEach(words => {
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.name = 'name_categorie';
-    checkbox.id = words.id;
+    // Seleccionar una cantidad específica de palabras para cada categoría
+    const numWordsPerCategory = 3;
+    const selectedWords = [];
 
-    const label = document.createElement('label');
-    label.htmlFor = words.id;
-    label.className = 'label-test';
-    label.appendChild(document.createTextNode(words.name));
-
-    // Agregar el checkbox y la etiqueta al contenedor
-    checkboxContainer.appendChild(checkbox);
-    checkboxContainer.appendChild(label);
+    // Obtener las palabras para cada categoría
+    categoriesData.forEach(category => {
+      fetch(`http://127.0.0.1:5001/api/v1/categories/${category.id}/words`)
+      .then(response => response.json())
+      .then(wordsData => {
+        // Obtener el contenedor de los checkboxes
+        const checkboxContainer = document.getElementById('checkbox-container');
+    
+        // Construir los checkboxes
+        wordsData.forEach(words => {
+          const checkbox = document.createElement('input');
+          checkbox.type = 'checkbox';
+          checkbox.name = 'name_categorie';
+          checkbox.id = words.id;
+    
+          const label = document.createElement('label');
+          label.htmlFor = words.id;
+          label.className = 'label-test';
+          label.appendChild(document.createTextNode(words.name));
+    
+          // Agregar el checkbox y la etiqueta al contenedor
+          checkboxContainer.appendChild(checkbox);
+          checkboxContainer.appendChild(label);
+        });
+      });
   });
+
 });
